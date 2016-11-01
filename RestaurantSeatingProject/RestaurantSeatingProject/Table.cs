873b5ca46fd930a7fb60b70575436b21cdc09af3
bgoff73 <bgoff73@BGOFFHP>
2016-11-01 11:17:32 -0500

    Bryan's Table and TableDA class

---
+++ RestaurantSeatingProject/RestaurantSeatingProject/Table.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSeatingProject
+{
+    class Table
+    {
+        private int size;
+        private string tableID;
+        //private Server server;
+        //private string lastName;
+        //private Group[] groups;
+
+        public Table()
+        {
+
+        }
+
+        public Table(string tableID, int size)
+        {
+            this.tableID = TheTable;
+            this.TheSize = TheSize;
+        }
+
+        public int TheSize
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public string TheTable
+        {
+            get { return tableID; }
+            set { tableID = value; }
+        }
+
+    }
+}
---
+++ RestaurantSeatingProject/RestaurantSeatingProject/TableDA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSeatingProject
+{
+    class TableDA
+    {
+        public static List<Table> GetAllTables()
+        {
+            List<Table> tables = new List<Table>();
+            Table table1 = new RestaurantSeatingProject.Table("1", 4);
+            Table table2 = new RestaurantSeatingProject.Table("2", 6);
+            Table table3 = new RestaurantSeatingProject.Table("3", 2);
+            Table table4 = new RestaurantSeatingProject.Table("4", 2);
+            Table table5 = new RestaurantSeatingProject.Table("5", 8);
+            Table table6 = new RestaurantSeatingProject.Table("6", 12);
+            Table table7 = new RestaurantSeatingProject.Table("7", 2);
+            Table table8 = new RestaurantSeatingProject.Table("8", 4);
+            Table table9 = new RestaurantSeatingProject.Table("9", 6);
+            Table table10 = new RestaurantSeatingProject.Table("10", 5);
+            Table table11 = new RestaurantSeatingProject.Table("11", 4);
+            Table table12 = new RestaurantSeatingProject.Table("12", 3);
+            Table table13 = new RestaurantSeatingProject.Table("13", 2);
+            Table table14 = new RestaurantSeatingProject.Table("14", 6);
+
+            tables.Add(table1);
+            return tables;
+        }
+
+
+    }
+}
