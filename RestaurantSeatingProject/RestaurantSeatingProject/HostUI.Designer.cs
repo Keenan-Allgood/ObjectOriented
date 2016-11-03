882118b8f266cd3c7e4629f0843c2fc47ad5e24b
Darrin <Darrin@LAPTOP1>
2016-11-03 11:22:01 -0500

    Added more GUI

--- RestaurantSeatingProject/RestaurantSeatingProject/HostUI.Designer.cs
+++ RestaurantSeatingProject/RestaurantSeatingProject/HostUI.Designer.cs
@@ -28,19 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabHost = new System.Windows.Forms.TabPage();
+            this.tabManager = new System.Windows.Forms.TabPage();
+            this.cboRestaurant = new System.Windows.Forms.ComboBox();
+            this.btnCreateLayout = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tabControl.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabHost);
+            this.tabControl.Controls.Add(this.tabManager);
+            this.tabControl.Location = new System.Drawing.Point(12, 48);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(964, 569);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabHost
+            // 
+            this.tabHost.BackColor = System.Drawing.Color.Transparent;
+            this.tabHost.Location = new System.Drawing.Point(4, 22);
+            this.tabHost.Name = "tabHost";
+            this.tabHost.Size = new System.Drawing.Size(956, 543);
+            this.tabHost.TabIndex = 0;
+            this.tabHost.Text = "HOST";
+            // 
+            // tabManager
+            // 
+            this.tabManager.Location = new System.Drawing.Point(4, 22);
+            this.tabManager.Name = "tabManager";
+            this.tabManager.Size = new System.Drawing.Size(956, 543);
+            this.tabManager.TabIndex = 1;
+            this.tabManager.Text = "MANAGER";
+            this.tabManager.UseVisualStyleBackColor = true;
+            // 
+            // cboRestaurant
+            // 
+            this.cboRestaurant.FormattingEnabled = true;
+            this.cboRestaurant.Location = new System.Drawing.Point(39, 16);
+            this.cboRestaurant.Name = "cboRestaurant";
+            this.cboRestaurant.Size = new System.Drawing.Size(249, 21);
+            this.cboRestaurant.TabIndex = 1;
+            // 
+            // btnCreateLayout
+            // 
+            this.btnCreateLayout.Location = new System.Drawing.Point(337, 16);
+            this.btnCreateLayout.Name = "btnCreateLayout";
+            this.btnCreateLayout.Size = new System.Drawing.Size(120, 24);
+            this.btnCreateLayout.TabIndex = 2;
+            this.btnCreateLayout.Text = "Create Layout";
+            this.btnCreateLayout.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(493, 16);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(120, 24);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete Layout";
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // HostUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(672, 498);
+            this.ClientSize = new System.Drawing.Size(988, 629);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCreateLayout);
+            this.Controls.Add(this.cboRestaurant);
+            this.Controls.Add(this.tabControl);
             this.Name = "HostUI";
             this.Text = "HostUI";
+            this.tabControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabHost;
+        private System.Windows.Forms.TabPage tabManager;
+        private System.Windows.Forms.ComboBox cboRestaurant;
+        private System.Windows.Forms.Button btnCreateLayout;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
