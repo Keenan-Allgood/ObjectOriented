daff2fbdef2ec2ed3362871b0243b56f1b180b8b
as560595 <as560595@LR00111>
2016-11-03 11:03:33 -0500

    Added Floor Plan Class

---
+++ RestaurantSeatingProject/RestaurantSeatingProject/FloorPlan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSeatingProject
+{
+    class FloorPlan
+    {
+        private int floorPlan;
+        private Table[] tables;
+
+        public FloorPlan(int aFloorPlan, Table[] aTables)
+        {
+            floorPlan = aFloorPlan;
+            tables = aTables;
+        }
+
+        public int Plan
+        {
+            get { return floorPlan; }
+            set { floorPlan = value;}
+        }
+        public Table[] Tables
+        {
+            get { return tables; }
+            set { tables = value; }
+        }
+    }
+}
