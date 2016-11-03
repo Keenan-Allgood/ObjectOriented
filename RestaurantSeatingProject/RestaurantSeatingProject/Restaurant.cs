cdc0779ff4c792c98bde27e972470fd3a433fa5a
as560595 <as560595@LR00111>
2016-11-03 11:17:19 -0500

    Restaurant class

---
+++ RestaurantSeatingProject/RestaurantSeatingProject/Restaurant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSeatingProject
+{
+    class Restaurant
+    {
+        private string name;
+        private string address;
+        private string manager;
+        private string host;
+
+        public Restaurant(string aName, string aAddress, string aManager, string aHost)
+        {
+            name = aName;
+            address = aAddress;
+            manager = aManager;
+            host = aHost;
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Manager { get; set; }
+        public string Host { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + " || Manager: " + Manager;
+        }
+
+    }
+}
