4ad2be86f1ea43ca764ee45afce0bf459287f9bc
Keenan <Keenan@KEENAN-PC>
2016-11-12 13:55:11 -0600

    Restaurant Layout Form

---
+++ RestaurantSeatingProject/RestaurantSeatingProject/RestaurantLayout.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace RestaurantSeatingProject
+{
+    partial class RestaurantLayout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmboTableList = new System.Windows.Forms.ComboBox();
+            this.btnDeleteTable = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.btnAddTable = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtManager = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmboTableList
+            // 
+            this.cmboTableList.FormattingEnabled = true;
+            this.cmboTableList.Location = new System.Drawing.Point(42, 133);
+            this.cmboTableList.Name = "cmboTableList";
+            this.cmboTableList.Size = new System.Drawing.Size(179, 21);
+            this.cmboTableList.TabIndex = 0;
+            // 
+            // btnDeleteTable
+            // 
+            this.btnDeleteTable.Location = new System.Drawing.Point(252, 131);
+            this.btnDeleteTable.Name = "btnDeleteTable";
+            this.btnDeleteTable.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteTable.TabIndex = 1;
+            this.btnDeleteTable.Text = "Delete";
+            this.btnDeleteTable.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Table Size:";
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(179, 194);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(61, 20);
+            this.txtSize.TabIndex = 3;
+            // 
+            // btnAddTable
+            // 
+            this.btnAddTable.Location = new System.Drawing.Point(252, 191);
+            this.btnAddTable.Name = "btnAddTable";
+            this.btnAddTable.Size = new System.Drawing.Size(75, 23);
+            this.btnAddTable.TabIndex = 4;
+            this.btnAddTable.Text = "Add Table";
+            this.btnAddTable.UseMnemonic = false;
+            this.btnAddTable.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(98, 248);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(239, 248);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Restaurant Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(125, 5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(169, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Address:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(125, 35);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(169, 20);
+            this.txtAddress.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Manager:";
+            // 
+            // txtManager
+            // 
+            this.txtManager.Location = new System.Drawing.Point(125, 61);
+            this.txtManager.Name = "txtManager";
+            this.txtManager.Size = new System.Drawing.Size(169, 20);
+            this.txtManager.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Host:";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(125, 85);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(169, 20);
+            this.txtHost.TabIndex = 14;
+            // 
+            // RestaurantLayout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 283);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtManager);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAddTable);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDeleteTable);
+            this.Controls.Add(this.cmboTableList);
+            this.Name = "RestaurantLayout";
+            this.Text = "RestaurantLayout";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmboTableList;
+        private System.Windows.Forms.Button btnDeleteTable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Button btnAddTable;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtManager;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtHost;
+    }
+}
